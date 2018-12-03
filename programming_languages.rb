84ed5579f0259c6486611d510fa3f823a09f3328
Joshua Alphonse <joshalphonse@gmail.com>
2018-12-03 21:19:47 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
+  {
+    :ruby=> {
+      :type => "interpreted"
+      :style => [:oo, :functional]
+    },
+    :javascript => {
+      :type => "interpreted"
+      :style => [:oo, :functional]
+      
+    }
+    
+  }
   # your code here
 end
