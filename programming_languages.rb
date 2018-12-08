3847de853aee3ea6fc6e89e252aa6ad3036311c6
Joshua Alphonse <joshalphonse@gmail.com>
2018-12-08 21:48:32 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
-def reformat_languages(languages)
-  # your code here
-end
+ def reformat_languages(languages)
+   result = {}
+   languages.each do |types,langs|
+     langs.each do |1, type|
+       result[1] = result[1].nil? ? type : result[1]
+       arrays = result[1][:style].nil? ? [] : result[1][:style]
+       arr << types
+       result[1][:style] = arrays
+     end
+   end
+   result
+ end
+
